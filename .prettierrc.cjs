520c3962f8fb36aea4d57edd7cdf099194b80c5e
Fabio Guardado <fabio.guardado2012@gmail.com>
2023-11-20 18:42:47 -0600

    :wrench: Config ESLint and Prettier

---
+++ .prettierrc.cjs
@@ -0,0 +1,12 @@
+/** @type {import("prettier").Config} */
+const config = {
+  endOfLine: "auto",
+  trailingComma: "es5",
+  tabWidth: 2,
+  semi: true,
+  singleQuote: true,
+  bracketSpacing: true,
+  arrowParens: "avoid"
+};
+
+module.exports = config;
