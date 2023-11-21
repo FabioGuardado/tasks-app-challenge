27b91838b0efd6c3d6ca54e8fa19ca4f1e41f4c0
Fabio Guardado <47063285+FabioGuardado@users.noreply.github.com>
2023-11-20 18:51:06 -0600

    :wrench: Config ESLint and Prettier (#1)

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,14 +2,22 @@ module.exports = {
   root: true,
   env: { browser: true, es2020: true },
   extends: [
-    'eslint:recommended',
+    'airbnb',
+    'airbnb-typescript',
+    'airbnb/hooks',
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
+    'plugin:prettier/recommended',
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  parserOptions: {
+    project: './tsconfig.json'
+  },
+  plugins: ['react-refresh', '@typescript-eslint', 'prettier'],
   rules: {
+    'react/react-in-jsx-scope': 0,
+    'import/no-absolute-path': 0,
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
