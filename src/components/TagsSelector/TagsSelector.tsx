7485c312662cde72353196d47e288c66c42b8996
Fabio Guardado <47063285+FabioGuardado@users.noreply.github.com>
2023-11-27 21:27:40 -0600

    :recycle: Improve tags selector (#12)

---
+++ src/components/TagsSelector/TagsSelector.tsx
@@ -0,0 +1,133 @@
+import { useState } from 'react';
+import { Icon } from '@iconify/react';
+
+import { Tags } from '../../interfaces/task';
+
+import './TagsSelector.scss';
+
+type PropsTypes = {
+  selectedLabels: Tags[];
+  handleListItemClick: (tagName: Tags) => void;
+};
+
+const TagsSelector = ({ selectedLabels, handleListItemClick }: PropsTypes) => {
+  const [isSubMenuOpen, setIsSubMenuOpen] = useState<boolean>();
+
+  const toggleSubMenu = () => setIsSubMenuOpen(!isSubMenuOpen);
+
+  return (
+    <div
+      role="button"
+      className="task-form__tags-selector"
+      tabIndex={0}
+      onClick={toggleSubMenu}
+      onKeyDown={toggleSubMenu}
+    >
+      <div className="task-form__tags-selector-button">
+        <span>Labels</span>
+        <Icon icon="iconamoon:arrow-down-2-duotone" />
+      </div>
+      <div
+        className={`task-form__tags-selector-dropdown ${
+          isSubMenuOpen
+            ? 'task-form__tags-selector-dropdown--visible'
+            : 'task-form__tags-selector-dropdown--hidden'
+        }`}
+      >
+        <ul className="dropdown-list" onMouseLeave={toggleSubMenu}>
+          <li>
+            <button
+              type="button"
+              className="list-item-button"
+              onClick={() => handleListItemClick(Tags.ANDROID)}
+            >
+              <Icon
+                height={16}
+                icon={`${
+                  selectedLabels.includes(Tags.ANDROID)
+                    ? 'mingcute:checkbox-line'
+                    : 'carbon:checkbox'
+                }`}
+              />
+              <span>ANDROID</span>
+            </button>
+          </li>
+
+          <li>
+            <button
+              type="button"
+              className="list-item-button"
+              onClick={() => handleListItemClick(Tags.IOS)}
+            >
+              <Icon
+                height={16}
+                icon={`${
+                  selectedLabels.includes(Tags.IOS)
+                    ? 'mingcute:checkbox-line'
+                    : 'carbon:checkbox'
+                }`}
+              />
+              <span>IOS</span>
+            </button>
+          </li>
+
+          <li>
+            <button
+              type="button"
+              className="list-item-button"
+              onClick={() => handleListItemClick(Tags.NODE_JS)}
+            >
+              <Icon
+                height={16}
+                icon={`${
+                  selectedLabels.includes(Tags.NODE_JS)
+                    ? 'mingcute:checkbox-line'
+                    : 'carbon:checkbox'
+                }`}
+              />
+              <span>NODE JS</span>
+            </button>
+          </li>
+
+          <li>
+            <button
+              type="button"
+              className="list-item-button"
+              onClick={() => handleListItemClick(Tags.RAILS)}
+            >
+              <Icon
+                height={16}
+                icon={`${
+                  selectedLabels.includes(Tags.RAILS)
+                    ? 'mingcute:checkbox-line'
+                    : 'carbon:checkbox'
+                }`}
+              />
+              <span>RAILS</span>
+            </button>
+          </li>
+
+          <li>
+            <button
+              type="button"
+              className="list-item-button"
+              onClick={() => handleListItemClick(Tags.REACT)}
+            >
+              <Icon
+                height={16}
+                icon={`${
+                  selectedLabels.includes(Tags.REACT)
+                    ? 'mingcute:checkbox-line'
+                    : 'carbon:checkbox'
+                }`}
+              />
+              <span>REACT</span>
+            </button>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default TagsSelector;
